fde53e4e703349a6776e8b3cda8d7b9dd713c203
JackAmbrose <jackambrose@gmail.com>
2017-01-02 01:44:49 -0800

    Initial Import

---
+++ campaign/scripts/char.lua
@@ -0,0 +1,12 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+function onInit()
+end
+
+function onClose()
+end
+
+function onMenuSelection(selection, subselection)
+end
